1ba6102542f8e0b4a2ab9b51f751677a3fcf9322
davvd <davvd@zold.io>
2026-01-01 12:52:28 +0300

    year up to 2026

--- src/test/java/io/zold/api/CopiesTest.java
+++ src/test/java/io/zold/api/CopiesTest.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2018-2025 Zerocracy
+ * SPDX-FileCopyrightText: Copyright (c) 2018-2026 Zerocracy
  * SPDX-License-Identifier: MIT
  */
 package io.zold.api;
